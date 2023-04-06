8c6235f38c9aff3cfafb7a9f27d687068e683ecc
Sina Nabigol <sina.nabigol77@gmail.com>
2023-04-06 12:47:41 +0330

    Edit blog index page

--- travelista/blog/admin.py
+++ travelista/blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from blog.models import Post
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    fields = ['title', 'content', 'status', 'publish_date']
+    list_display = ('title', 'counted_view', 'status', 'publish_date', 'created_date')
+    list_filter = ['status', 'created_date']
+    search_fields = ['title', 'content']
+    ordering = ['-created_date']
--- travelista/blog/views.py
+++ travelista/blog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from blog.models import Post
+from datetime import datetime
 
 # Create your views here.
 def blog_index_view(request):
-    return render(request, 'blog/blog-index.html')
+    posts = Post.objects.filter(status=1, publish_date__lte=datetime.now())
+    context = {'posts': posts};
+    return render(request, 'blog/blog-index.html', context)
 
 def blog_single_view(request):
     return render(request, 'blog/blog-single.html')
