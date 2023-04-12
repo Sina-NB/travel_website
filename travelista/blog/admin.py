ce96f339af2ceac8582ca03f95d5c9e660de552f
Sina Nabigol <sina.nabigol77@gmail.com>
2023-04-12 16:18:53 +0330

    Add Category to blog admin

--- travelista/blog/admin.py
+++ travelista/blog/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post, Category
 
 # Register your models here.
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    fields = ['title', 'content', 'status', 'publish_date']
-    list_display = ('title', 'counted_view', 'status', 'publish_date', 'created_date')
+    fields = ['title', 'image', 'author', 'category', 'content', 'status', 'publish_date']
+    list_display = ('title', 'author' ,'counted_view', 'status', 'publish_date', 'created_date')
     list_filter = ['status', 'created_date']
     search_fields = ['title', 'content']
     ordering = ['-created_date']
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    fields = ['name']
